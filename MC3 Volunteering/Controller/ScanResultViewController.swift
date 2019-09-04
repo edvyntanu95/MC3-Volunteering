724020f378e56be9b7d34bd5ee9c97fdbf50d832
Trevin <trevin.john.rutherford@gmail.com>
2019-09-04 14:46:33 +0700

    scanner page UI completed

--- MC3 Volunteering/Controller/ScanResultViewController.swift
+++ MC3 Volunteering/Controller/ScanResultViewController.swift
@@ -13,7 +13,10 @@ class ScanResultViewController: UIViewController {
     @IBOutlet weak var ivPersonImage: UIImageView!
     @IBOutlet weak var lblName: UILabel!
     @IBOutlet weak var addFriendButton: UIButton!
+    @IBOutlet weak var viewBackground: UIView!
+    @IBOutlet weak var lblLocation: UILabel!
     
+    @IBOutlet weak var lblNickName: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,6 +25,15 @@ class ScanResultViewController: UIViewController {
     
     func setUpPage(){
         ivPersonImage.layer.cornerRadius = ivPersonImage.frame.height / 2
+        ivPersonImage.layer.masksToBounds = true
+        
+        addFriendButton.layer.cornerRadius = 20
+        addFriendButton.layer.borderColor = #colorLiteral(red: 0.08758807927, green: 0.5526862144, blue: 0.8551954627, alpha: 1)
+        addFriendButton.layer.borderWidth = 0.5
+        
+        viewBackground.backgroundColor = #colorLiteral(red: 0.08758807927, green: 0.5526862144, blue: 0.8551954627, alpha: 1)
+        viewBackground.layer.cornerRadius = 10
+        viewBackground.layer.masksToBounds = true
     }
 
 }
